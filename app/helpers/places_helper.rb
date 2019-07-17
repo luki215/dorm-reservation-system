dc76119c26c90575264ced7e20ebfebf474b039c
root <lukas.brezina@coex.cz>
2019-07-17 13:11:19 +0000

    feat(places): show free spaces count

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -2,4 +2,16 @@ module PlacesHelper
     def room_full(place)
         return place.building + place.floor.to_s + place.room
     end
+
+    def free_spaces_count_class(places_count)
+        ratio = places_count.free.to_f/places_count.all
+
+        if ratio > 0.5 
+            'success'
+        elsif ratio > 0
+            'warning'
+        else 
+            'danger'
+        end
+    end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,24 @@ class Place < ApplicationRecord
     #TODO implement availability logic
     return self.user.nil?
   end
+
+  def self.places_count(building, floor = nil, cell = nil)
+    places = self.where(building: building)
+    places = places.where(floor: floor) if floor
+    places = places.where(cell: cell) if cell
+
+    Rails.cache.fetch(places.cache_key) do
+      count = PlacesCount.new(places.where(user_id: nil).count, places.count)
+      count
+    end
+  end
+end
+
+class PlacesCount
+  attr_reader :free, :all
+
+  def initialize(free, all)
+    @free = free 
+    @all = all
+  end
 end
