8d38daf4a0113e0d0bb3a788421133c8ab000fba
Lukas Brezina <lukas.brezina@coex.cz>
2019-08-08 17:50:49 +0000

    feat(app-settings): current round helper method

--- app/models/app_setting.rb
+++ app/models/app_setting.rb
@@ -1,2 +1,11 @@
 class AppSetting < ApplicationRecord
+
+    def current_round
+        :before if DateTime.now < first_round_start
+        :first if DateTime.now < second_round_start
+        :second if DateTime.now < third_round_start
+        :third if DateTime.now < fourth_round_start
+        :fourth
+        
+    end
 end
