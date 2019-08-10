cf88cecc1b2a8cfcd1a886ff438f2913604746c0
Viki Nemecek <ryba@divocaci.cz>
2019-08-10 03:56:37 +0200

    Kola zámluv

--- app/models/app_setting.rb
+++ app/models/app_setting.rb
@@ -13,4 +13,18 @@ class AppSetting < ApplicationRecord
           return :fourth
         end  
     end
+
+    def current_round_numeric
+        if DateTime.now < first_round_start
+          return 0
+        elsif DateTime.now < second_round_start
+          return 1
+        elsif DateTime.now < third_round_start
+          return 2
+        elsif DateTime.now < fourth_round_start
+          return 3
+        else
+          return 4
+        end  
+    end
 end
