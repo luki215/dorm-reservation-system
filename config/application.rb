db1f92a0287c39bc6d2158a05482d5e217a097a8
Lukas Brezina <lukas.brezina@coex.cz>
2019-08-21 10:02:42 +0000

    feat(timezone): change to prague timezone

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module App
     config.i18n.default_locale = :cs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = "Prague"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
